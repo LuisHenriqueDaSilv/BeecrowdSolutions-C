4c727d306c425fc12b8b77001afdb8f449e95335
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2025-01-18 14:15:55 -0300

    Iniciando

---
+++ 1629/index.c
@@ -0,0 +1,13 @@
+int main(){
+
+  int n;
+  while(1){
+    scanf("%d", &n);
+    if( n == 0){break;}
+    char input[1000];
+    for(int i =0; i<n; i++){
+      scanf("%s", input);
+      if()
+    }
+  };
+}
